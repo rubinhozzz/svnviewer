c6d027f978328d1fc32e9ea2a7f459c4b66d0346
Ruben Sanchez <ruben.flex@gmail.com>
2013-12-11 16:05:55 +0100

    Fix coding issues
    Signed-off-by: Ruben Sanchez <ruben.flex@gmail.com>

--- SVNViewer.py
+++ SVNViewer.py
@@ -7,46 +7,49 @@
     @copyright: 2013 Ruben Sanchez <ruben.flex@gmail.com>,
     @license: GNU GPL, see COPYING for details.
 """
-from cStringIO import StringIO
+import StringIO
 from pysvn import wc_status_kind, opt_revision_kind, wc_notify_action, depth
 import pysvn
 from MoinMoin import wikiutil
 from MoinMoin.parser.highlight import Parser
 
 def macro_SVNViewer(macro, url, file_type='text', revision_number='HEAD', start=None, end=None):
-    result = ''
     client = pysvn.Client()
-    if is_number(revision_number):
-        revision = pysvn.Revision(pysvn.opt_revision_kind.number, int(revision_number))
-    else: 
+    try:
+        if revision_number.strip().upper() == 'HEAD':
+            revno = client.info2(url)[0][1].rev.number
+        else:
+            revno = int(revision_number)
+    except:
         revision = pysvn.Revision(pysvn.opt_revision_kind.head)
+    else:
+        revision = pysvn.Revision(pysvn.opt_revision_kind.number, revno)
     output = client.cat(url, revision)
-    out = StringIO(output)
+    out = StringIO.StringIO(output)
     lines = out.readlines()
     x, y = clean_range(start, end, len(lines))
-    result += u''.join(lines[x-1:y])
+    result = u''.join(lines[x-1:y])
     parser = Parser(result, macro.request, format_args=file_type)
     parser.num_start = x
     parser.format(macro.request.formatter)
     del out
-    return ''
+    return u''
 
 def clean_range(start, end, limit):
-    #cleaning start
-    if start is None or is_number(start) == False or int(start) > limit:
+    try:
+        start = int(start)
+    except:
         start = 1
     else:
-        start = int(start)
-    #cleaning end
-    if end is None or is_number(end) == False or int(end) > limit or int(end) < start:
+        if start > limit:
+            start = 1
+
+    try:
+        end = int(end)
+    except:
         end = limit
     else:
-        end = int(end)
-    return start, end
+        if end > limit or end < start:
+            end = limit
 
-def is_number(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+    return start, end
